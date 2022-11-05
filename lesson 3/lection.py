0ecb2f0fd8f06da25a182f9382e86efa9993b3a8
max <naaahhbro@gmail.com>
2022-11-05 19:19:00 +0400

    урок 3

---
+++ lesson 3/lection.py
@@ -0,0 +1,31 @@
+# my_fun = lambda p1, p2: p1 + p2
+#
+# print((lambda *args: args) (2, 5, 'sa'))
+# def my_f():
+#     global m_1, m_2
+#     num1 = 67
+#     num2 = 87
+#     m_1 = num1 * num2 - number
+#     m_2 = num1 ** num2
+#     return m_1, m_2
+#
+# number = int(input("vvod chisla"))
+# print(my_f())
+# print(m_1)
+# """def fun():
+#     m1 = 5
+#     def in_f():
+#         nonlocal m1
+#         m1 += 1
+#         return m1
+#     return in_f
+# f = fun()
+# print(f())
+# """
+p = input("Enter nubmers - ")
+try:
+    n = int(p)
+    print(p)
+except ValueError:
+    print("Only numbers")
+
---
+++ lesson 3/task1.py
@@ -0,0 +1,1 @@
+def fun_del():
