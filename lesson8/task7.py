74f241cb92e6433940db257827272cf87da86265
Max <bezonmax@gmail.com>
2023-01-05 01:56:23 +0400

    lesson8

---
+++ lesson8/task7.py
@@ -0,0 +1,27 @@
+# калькулятор для проверки https://ru.onlinemschool.com/math/assistance/complex_number/calculation/
+class ComplexNum:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __add__(self, other):
+        syntax = ''
+        if self.b + other.b >= 0:  # если b отрицательное
+            syntax = '+'
+        elif self.b + other.b < 0:
+            syntax = '-'
+        return f'{self.a + other.a}{syntax}{abs(self.b + other.b)}i'
+
+    def __mul__(self, other):
+        syntax = ''
+        if self.a * other.b + self.b * other.a >= 0:
+            syntax = '+'
+        elif self.a * other.b + self.b * other.a < 0:
+            syntax = '-'
+        return f'{self.a * other.a - self.b * other.b}{syntax}{abs(self.a * other.b + self.b * other.a)}i'
+
+
+z1 = ComplexNum(10, -5255)
+z2 = ComplexNum(-15, -10)
+z = z1 * z2
+print(z)
